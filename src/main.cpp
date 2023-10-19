470d5fead0e6e4728b301d7f63375d7645e14164
GamingRex4274 <grex4274@gmail.com>
2023-10-18 21:28:41 -0400

    FINALLY GOT THE CMAKE SFML TEMPLATE WORKING.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <SFML/Graphics.hpp>
+#include <random>
+#include <iostream>
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Abstract Data Taip");
+    window.setFramerateLimit(5);
+    sf::CircleShape shape(200.f);
+
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_int_distribution<int> dist(0, 255);
 
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
 
         window.clear();
+
+        sf::Color c = sf::Color(dist(rng), dist(rng), dist(rng));
+        std::cout << c.r << " " << c.g << " " << c.b << "\n";
+        shape.setFillColor(c);
+
+        window.draw(shape);
         window.display();
     }
+
+    return 0;
 }
